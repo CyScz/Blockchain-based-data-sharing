52189ea28711f650db9b334fb3e7fe2a756d8e91
Frank Théodoloz <frank.theodoloz@etu.hesge.ch>
2022-09-06 18:29:38 +0200

    New utils.sh file

---
+++ install-scripts/utils.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+Color_Off='\e[0m'   # Text Reset
+BIYellow='\e[1;93m' # Yellow
+BIRed='\e[1;91m'    # Red
+BIGreen='\e[1;92m'  # Green
+
+## log [TEXT, COLOR]
+#  Prints the text in color
+function log {
+  echo -e "${2}${1}${Color_Off}"
+}
+
+## checkRoot [true|false]
+#  Exits if it does not meet condition
+function checkRoot {
+  if [ ! "$EUID" -eq 0 ] && [ $1 = true ]; then
+    log "Script must be run as root user eg.\n sudo ./${0##*/}" $BIRed
+    kill -s TERM $TOP_PID
+  elif [ "$EUID" -eq 0 ] && [ $1 = false ]; then
+    log "Script must be run as normal user eg.\n ./${0##*/}" $BIRed
+    kill -s TERM $TOP_PID
+  fi
+}
+
+## checkGroupMembership [Group]
+# Exit if user is not part of group
+function checkGroupMembership {
+  if ! (id | grep $1); then
+    log "'$USER' does not have required permissions, please log out to apply" $BIRed
+    kill -s TERM $TOP_PID
+  fi
+}
+
+## checkInstall [programName, testCommand]
+#  Checks whether the result of testCommand outputs something
+function checkInstall {
+  if ! command -v $2 &>/dev/null; then
+    echo "$1 installation error" $BIRed
+    kill -s TERM $TOP_PID
+  fi
+  log "$1 installation completed \n$(eval $2)" $BIGreen
+}
