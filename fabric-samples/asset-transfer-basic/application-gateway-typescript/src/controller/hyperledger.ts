ddf9b5dd0eca8fd830d895b7fb6d1ccaf9d58e31
Frank Théodoloz <frank.theodoloz@etu.hesge.ch>
2022-09-21 22:07:15 +0200

    New Hyperledger controller based on app.ts

---
+++ fabric-samples/asset-transfer-basic/application-gateway-typescript/src/controller/hyperledger.ts
@@ -0,0 +1,218 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as grpc from '@grpc/grpc-js';
+import {connect, Contract, Identity, Signer, signers} from '@hyperledger/fabric-gateway';
+import * as crypto from 'crypto';
+import {promises as fs} from 'fs';
+import * as path from 'path';
+import {TextDecoder} from 'util';
+import {Asset} from '../model/Asset.js';
+
+import {fileURLToPath} from 'url';
+import {unitSize} from '../utils/sizeUtils.js';
+
+const channelName = envOrDefault('CHANNEL_NAME', 'mychannel');
+const chaincodeName = envOrDefault('CHAINCODE_NAME', 'basic');
+const mspId = envOrDefault('MSP_ID', 'Org1MSP');
+
+// https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Path to crypto materials.
+const cryptoPath = envOrDefault('CRYPTO_PATH', path.resolve(__dirname, '..', '..', '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com'));
+
+// Path to user private key directory.
+const keyDirectoryPath = envOrDefault('KEY_DIRECTORY_PATH', path.resolve(cryptoPath, 'users', 'User1@org1.example.com', 'msp', 'keystore'));
+
+// Path to user certificate.
+const certPath = envOrDefault('CERT_PATH', path.resolve(cryptoPath, 'users', 'User1@org1.example.com', 'msp', 'signcerts', 'cert.pem'));
+
+// Path to peer tls certificate.
+const tlsCertPath = envOrDefault('TLS_CERT_PATH', path.resolve(cryptoPath, 'peers', 'peer0.org1.example.com', 'tls', 'ca.crt'));
+
+// Gateway peer endpoint.
+const peerEndpoint = envOrDefault('PEER_ENDPOINT', 'localhost:7051');
+
+// Gateway peer SSL host name override.
+const peerHostAlias = envOrDefault('PEER_HOST_ALIAS', 'peer0.org1.example.com');
+
+const utf8Decoder = new TextDecoder();
+const assetId = `asset${Date.now()}`;
+
+async function addAsset(asset: Asset): Promise<void> {
+    // await displayInputParameters();
+
+    // The gRPC client connection should be shared by all Gateway connections to this endpoint.
+    const client = await newGrpcConnection();
+    const gateway = connect({
+        client,
+        identity: await newIdentity(),
+        signer: await newSigner(),
+        evaluateOptions: () => {
+            return {deadline: Date.now() + 5000}; // 5 seconds
+        },
+        endorseOptions: () => {
+            return {deadline: Date.now() + 15000}; // 15 seconds
+        },
+        submitOptions: () => {
+            return {deadline: Date.now() + 5000}; // 5 seconds
+        },
+        commitStatusOptions: () => {
+            return {deadline: Date.now() + 60000}; // 1 minute
+        },
+    });
+
+    try {
+        // Get a network instance representing the channel where the smart contract is deployed.
+        const network = gateway.getNetwork(channelName);
+
+        // Get the smart contract from the network.
+        const contract = network.getContract(chaincodeName);
+
+        // Create a new asset on the ledger.
+        await createAsset(contract, asset);
+
+    } finally {
+        gateway.close();
+        client.close();
+    }
+}
+
+async function getAssets(): Promise<void> {
+    // await displayInputParameters();
+
+    // The gRPC client connection should be shared by all Gateway connections to this endpoint.
+    const client = await newGrpcConnection();
+    const gateway = connect({
+        client,
+        identity: await newIdentity(),
+        signer: await newSigner(),
+        // Default timeouts for different gRPC calls
+        evaluateOptions: () => {
+            return {deadline: Date.now() + 5000}; // 5 seconds
+        },
+        endorseOptions: () => {
+            return {deadline: Date.now() + 15000}; // 15 seconds
+        },
+        submitOptions: () => {
+            return {deadline: Date.now() + 5000}; // 5 seconds
+        },
+        commitStatusOptions: () => {
+            return {deadline: Date.now() + 60000}; // 1 minute
+        },
+    });
+
+    try {
+        // Get a network instance representing the channel where the smart contract is deployed.
+        const network = gateway.getNetwork(channelName);
+
+        // Get the smart contract from the network.
+        const contract = network.getContract(chaincodeName);
+
+        // Create a new asset on the ledger.
+        await getAllAssets(contract);
+
+    } finally {
+        gateway.close();
+        client.close();
+    }
+}
+
+async function newGrpcConnection(): Promise<grpc.Client> {
+    const tlsRootCert = await fs.readFile(tlsCertPath);
+    const tlsCredentials = grpc.credentials.createSsl(tlsRootCert);
+
+    return new grpc.Client(peerEndpoint, tlsCredentials, {
+        'grpc.ssl_target_name_override': peerHostAlias,
+    });
+}
+
+async function newIdentity(): Promise<Identity> {
+    const credentials = await fs.readFile(certPath);
+    return {mspId, credentials};
+}
+
+async function newSigner(): Promise<Signer> {
+    const files = await fs.readdir(keyDirectoryPath);
+    const keyPath = path.resolve(keyDirectoryPath, files[0]);
+    const privateKeyPem = await fs.readFile(keyPath);
+    const privateKey = crypto.createPrivateKey(privateKeyPem);
+    return signers.newPrivateKeySigner(privateKey);
+}
+
+/**
+ * Submit a transaction synchronously, blocking until it has been committed to the ledger.
+ */
+async function createAsset(contract: Contract, asset: Asset): Promise<void> {
+    console.log(`\n--> Submit Transaction: CreateAsset, split ${asset.SplitRatio}, size: ${(Buffer.from(asset.ChunkAData).length / unitSize.MB)
+        .toLocaleString(undefined, {minimumFractionDigits: 2})} MB`);
+
+    // CreateAsset(ctx: Context, id: string, filename: string, size: number, hash: string, sender: string,
+    //                              splitRatio: number, chunkAHash: string, chunkAData: string,
+    //                              chunkBHash: string, chunkBIpfsCid: string): Promise<void>
+    await contract.submitTransaction(
+        'CreateAsset',
+        asset.ID,
+        asset.Filename,
+        asset.Size.toString(),
+        asset.Hash,
+        asset.Sender,
+        asset.SplitRatio.toString(),
+        asset.ChunkAHash,
+        asset.ChunkAData,
+        asset.ChunkBHash,
+        asset.ChunkBIpfsCid
+    );
+
+    console.log('*** Transaction committed successfully');
+}
+
+/**
+ * Evaluate a transaction to query ledger state.
+ */
+async function getAllAssets(contract: Contract): Promise<void> {
+    console.log('\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger');
+
+    const resultBytes = await contract.evaluateTransaction('GetAllAssets');
+    const resultJson = utf8Decoder.decode(resultBytes);
+
+    const result = JSON.parse(resultJson);
+
+    const arr: Asset[] = []
+    for (const resultElement of result) {
+        arr.push(new Asset(resultElement))
+    }
+
+    for (const asset1 of arr) {
+        console.log(asset1.toString())
+    }
+}
+
+/**
+ * envOrDefault() will return the value of an environment variable, or a default value if the variable is undefined.
+ */
+function envOrDefault(key: string, defaultValue: string): string {
+    return process.env[key] || defaultValue;
+}
+
+/**
+ * displayInputParameters() will print the global scope parameters used by the main driver routine.
+ */
+async function displayInputParameters(): Promise<void> {
+    console.log(`channelName:       ${channelName}`);
+    console.log(`chaincodeName:     ${chaincodeName}`);
+    console.log(`mspId:             ${mspId}`);
+    console.log(`cryptoPath:        ${cryptoPath}`);
+    console.log(`keyDirectoryPath:  ${keyDirectoryPath}`);
+    console.log(`certPath:          ${certPath}`);
+    console.log(`tlsCertPath:       ${tlsCertPath}`);
+    console.log(`peerEndpoint:      ${peerEndpoint}`);
+    console.log(`peerHostAlias:     ${peerHostAlias}`);
+}
+
+export {addAsset, getAssets}
