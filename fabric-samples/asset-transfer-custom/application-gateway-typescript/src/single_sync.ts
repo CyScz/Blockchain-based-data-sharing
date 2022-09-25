573d60176e56ea9012a78551699137f0756a742d
Frank Théodoloz <frank.theodoloz@etu.hesge.ch>
2022-09-25 22:43:17 +0200

    New single_sync with promises replacing index_sync

---
+++ fabric-samples/asset-transfer-custom/application-gateway-typescript/src/single_sync.ts
@@ -0,0 +1,101 @@
+/**
+ * single_sync : Processes a single file, multiple times, once per split ratio
+ * Script does WAIT until the upload completes before processing next ratio ->> ordered
+ *
+ * This node app takes 1 parameter (example usage : npm run single-sync 10)
+ *  - filesize : size of the file in MB
+ */
+
+import * as Path from 'path';
+import * as crypto from 'crypto'
+import fs from 'fs';
+import {exec} from 'child_process'
+import {promisify} from 'util';
+import * as ipfs from './controller/ipfs.js';
+import * as hl from './controller/hyperledger.js';
+import {getDateTimeString, getTimeWithMs} from './utils/dateUtils.js';
+import log from './utils/fileLog.js';
+
+const execPromise = promisify(exec);
+
+let fileSize = 10;
+const splitSizes: number[] = [.1, .2, .3, .4, .5, .7, .8, .9]
+const args = process.argv.slice(2);
+
+if (args.length > 0) {
+    fileSize = Number.parseInt(args[0]);
+    console.log(`filesize: ${fileSize}`);
+}
+
+const resultsFileName = 'results_' + getDateTimeString(new Date(Date.now())) + '_single_sync.csv';
+
+// csv header
+log(resultsFileName, 'Split,Total size (B),Off-chain size (B),Off-chain start time,Off-chain end time,Off-chain Duration (ms),On-chain size (B),On-chain start time,On-chain end time,On-chain duration (ms)\r\n');
+
+function getHash(buffer: Buffer): string {
+    const hash = crypto.createHash('md5');
+    hash.update(buffer);
+    return hash.digest('hex');
+}
+
+async function generateFiles(size: number): Promise<void> {
+    console.log(`Generating test file of ${size}MB`)
+    await execPromise(`./generateFile.sh 1 ${size} ./`);
+}
+
+async function main(): Promise<void> {
+    await generateFiles(fileSize);
+
+    for (const splitSize of splitSizes) {
+        console.log(`processing split ${splitSize}`)
+
+        const testFilePath: string = Path.resolve('.', 'testFile0.bin');
+        const fileSize: number = fs.statSync(testFilePath).size;
+        const fileBuffer: Buffer = fs.readFileSync(testFilePath);
+
+        // split file
+        const onChainData: Buffer = fileBuffer.subarray(0, fileSize * splitSize); // 0 >> n
+        const onChainSize_base64 = Buffer.from(onChainData.toString('base64')).byteLength;
+        const offChainData: Buffer = fileBuffer.subarray(fileSize * splitSize); // n >> end
+        const offChainSize_base64 = Buffer.from(offChainData.toString('base64')).byteLength;
+
+        const startTime = Date.now()
+        const cid = await ipfs.addFile(Path.basename(testFilePath), offChainData);
+        const offChainEndTime = Date.now()
+
+        console.log(`Split: ${splitSize}, IPFS duration: ${offChainEndTime - startTime} ms`);
+
+        await hl.addAsset({
+            ID: startTime.toString(),
+            Filename: Path.basename(testFilePath),
+            Size: fileSize,
+            Hash: getHash(fileBuffer),
+            Sender: 'application-gateway-typescript',
+            SplitRatio: splitSize,
+            OnChainHash: getHash(onChainData),
+            OnChainData: onChainData.toString('base64'),
+            OffChainHash: getHash(offChainData),
+            OffChainIpfsCid: cid.toString()
+        });
+        const onChainEndTime = Date.now();
+
+        console.info(`Split: ${splitSize} Hyperledger duration: ${onChainEndTime - offChainEndTime} ms`);
+        console.info(`Split: ${splitSize} Total duration: ${onChainEndTime - startTime} ms`);
+        log(resultsFileName, `${splitSize},${fileSize},${offChainSize_base64},${getTimeWithMs(new Date(startTime))},${getTimeWithMs(new Date(offChainEndTime))},${offChainEndTime - startTime},${onChainSize_base64},${getTimeWithMs(new Date(offChainEndTime))},${getTimeWithMs(new Date(onChainEndTime))},${onChainEndTime - offChainEndTime}\r\n`);
+    }
+}
+
+main()
+    .catch(error => {
+        console.error('******** FAILED to run the application:', error);
+        process.exitCode = 1;
+    });
+
+
+
+
+
+
+
+
+
