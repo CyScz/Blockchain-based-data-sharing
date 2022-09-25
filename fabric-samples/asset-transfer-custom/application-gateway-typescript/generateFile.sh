938f201f4501f4e63c8757e8d9d0583f64945e2f
Frank Théodoloz <frank.theodoloz@etu.hesge.ch>
2022-09-25 22:35:43 +0200

    Improved generateFile with parameters for multiple files

--- fabric-samples/asset-transfer-custom/application-gateway-typescript/generateFile.sh
+++ fabric-samples/asset-transfer-custom/application-gateway-typescript/generateFile.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-dd if=/dev/urandom of=${2-testFile.bin} bs=1024000 count=${1-10}
+# this script generates (a) binary file(s) with random data
+# command-line arguments [quantity, size, filepath]
+# Default : 1 file of 10 MB in this folder
+
+quantity=${1- 1};
+size=${2-10};
+filepath=${3-"./"};
+
+echo "Creating ${quantity} file(s) of ${size}MB in ${filepath}";
+
+for ((i=0; i < quantity; i++)); do
+  filename=${filepath}/testFile${i}.bin;
+  dd if=/dev/urandom of="${filename}" bs=1024000 count=${size-10};
+done
