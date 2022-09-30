891e04402eceb204dbbbf1ce63eb8b487d484f3a
Frank Théodoloz <frank.theodoloz@etu.hesge.ch>
2022-09-30 02:00:56 +0200

    Corrected buffer encoding

--- fabric-samples/asset-transfer-custom/application-gateway-typescript/src/controller/ipfs.ts
+++ fabric-samples/asset-transfer-custom/application-gateway-typescript/src/controller/ipfs.ts
@@ -2,7 +2,7 @@ import {create} from 'ipfs-http-client'
 
 const addFile = async (fileName: string, fileBuffer: Buffer) => {
     const ipfs = create({url: 'http://' + process.env.IPFS_PEER_IP + ':' + process.env.IPFS_PEER_PORT});
-    const fileAdded = await ipfs.add({path: fileName, content: fileBuffer});
+    const fileAdded = await ipfs.add({path: fileName, content: fileBuffer.toString('base64')});
     const {cid} = fileAdded;
     return cid;
 }
