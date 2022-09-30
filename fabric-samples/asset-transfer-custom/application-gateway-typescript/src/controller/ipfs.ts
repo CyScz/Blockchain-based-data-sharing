d67201db7cc7d8a48c63f3de3026fa2d314ac3c4
Frank Théodoloz <frank.theodoloz@etu.hesge.ch>
2022-09-30 02:00:38 +0200

    New getFile function

--- fabric-samples/asset-transfer-custom/application-gateway-typescript/src/controller/ipfs.ts
+++ fabric-samples/asset-transfer-custom/application-gateway-typescript/src/controller/ipfs.ts
@@ -7,5 +7,16 @@ const addFile = async (fileName: string, fileBuffer: Buffer) => {
     return cid;
 }
 
-export {addFile}
+const getFile = async (cid: string) => {
+    const ipfs = create({url: 'http://' + process.env.IPFS_PEER_IP + ':' + process.env.IPFS_PEER_PORT});
 
+    const stream = ipfs.cat(cid);
+    let ipfsData = ''
+    for await (const chunk of stream) {
+        // chunks of data are returned as a Buffer, convert it back to a string
+        ipfsData += chunk.toString();
+    }
+    return Buffer.from(ipfsData, 'base64');
+}
+
+export {addFile, getFile}
