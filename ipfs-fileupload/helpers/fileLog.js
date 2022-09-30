93272910b136ed637afc8c9c49fd1fb45982aa66
Frank Théodoloz <frank.theodoloz@etu.hesge.ch>
2022-09-30 02:24:53 +0200

    Deleted former Javascript IPFS app

--- ipfs-fileupload/helpers/fileLog.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-export default function log(data) {
-    // append data to file
-    fs.appendFile('log.txt', data, 'utf8',
-        function (err) {
-            if (err) throw err;
-        });
-}
